1bd8a216f6b99d3395b4bd23c8f0e9c4e2da16b9
jindalujjwal0720 <jindalujjwal0720@gmail.com>
2024-12-10 13:12:04 +0530

    Update Jest and TypeScript configurations to exclude additional directories from test paths

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -164,7 +164,12 @@ const config: Config = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  testPathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+    '\\\\dist\\\\',
+    '\\\\build\\\\',
+    '\\\\coverage\\\\',
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
