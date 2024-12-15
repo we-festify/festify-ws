5da3e710189a6ef3440e84f41972035d9e8268b6
jindalujjwal0720 <jindalujjwal0720@gmail.com>
2024-12-15 15:20:58 +0530

    Add DB_ENDPOINT configuration for MongoDB connection

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -16,6 +16,7 @@ export default {
     host: process.env.DB_HOST ?? 'localhost',
     port: process.env.DB_PORT ?? 27017,
     name: process.env.DB_NAME ?? 'something',
+    endpoint: process.env.DB_ENDPOINT,
   },
   redis: {
     host: process.env.REDIS_HOST ?? 'localhost',
--- apps/server/src/loaders/mongoose.ts
+++ apps/server/src/loaders/mongoose.ts
@@ -2,6 +2,10 @@ import * as mongoose from 'mongoose';
 import { env } from '../config';
 
 const mongooseLoader = async () => {
+  if (env.db.endpoint) {
+    return await mongoose.connect(env.db.endpoint);
+  }
+
   const connection = await mongoose.connect(
     `mongodb://${env.db.host}:${env.db.port}/${env.db.name}`,
   );
