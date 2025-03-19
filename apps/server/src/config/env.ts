4beca30e8641bf04abbc5f66aedd4d43c9457c86
jindalujjwal0720 <jindalujjwal0720@gmail.com>
2025-03-19 16:43:51 +0530

    Rename PORT to SERVER_PORT in .env.example and update env.ts to reflect the change

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export default {
   nodeEnv: process.env.NODE_ENV ?? 'development',
-  port: process.env.PORT ?? 5000,
+  port: process.env.SERVER_PORT ?? 5000,
   url: process.env.URL ?? 'http://localhost:5000',
   db: {
     host: process.env.DB_HOST ?? 'localhost',
